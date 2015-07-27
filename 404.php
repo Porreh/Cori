1fd26483ca53841f4e053004401b726538668e3b
Dmytro <Porreh@gmail.com>
2015-07-27 09:21:27 +0300

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Cori</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <section id="404">
+      <div>
+        <img src="<?php echo theme_url($append = '/img/image.#'); ?>" />
+        <span><p>Can't find the page. Go back to <a href="<?php echo base_url(); ?>"><?php echo site_name(); ?></a>.</p></span>
+      </div>
+    </section>
+  </body>
+</html>
